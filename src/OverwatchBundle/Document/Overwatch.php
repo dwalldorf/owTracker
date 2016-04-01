ee7593a60c372a73a93797cc378d2cce82ff4915
Daniel Walldorf <d.walldorf@me.com>
2016-04-01 19:48:08 +0200

    Overwatch entity

--- src/OverwatchBundle/Document/Overwatch.php
+++ src/OverwatchBundle/Document/Overwatch.php
@@ -9,6 +9,20 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
  */
 class Overwatch {
 
+    private $mapPool = [
+        0  => 'de_dust2',
+        1  => 'de_inferno',
+        2  => 'de_nuke',
+        3  => 'de_train',
+        4  => 'de_mirage',
+        5  => 'de_cache',
+        6  => 'de_cbbl',
+        7  => 'de_overpass',
+        8  => 'de_tuscan',
+        9  => 'de_season',
+        10 => 'de_santorini',
+    ];
+
     /**
      * @ODM\Id
      * @var string
@@ -22,6 +36,12 @@ class Overwatch {
     private $userId;
 
     /**
+     * @ODM\Date(nullable=false)
+     * @var \DateTime
+     */
+    private $creationDate;
+
+    /**
      * @ODM\String(nullable=false)
      * @var string
      */
@@ -31,25 +51,29 @@ class Overwatch {
      * @ODM\Bool(nullable=false)
      * @var bool
      */
-    private $aimAssist;
+    private $aimAssist = false;
 
     /**
      * @ODM\Bool(nullable=false)
      * @var bool
      */
-    private $visionAssist;
+    private $visionAssist = false;
 
     /**
      * @ODM\Bool(nullable=false)
      * @var bool
      */
-    private $otherAssist;
+    private $otherAssist = false;
 
     /**
      * @ODM\Bool(nullable=false)
      * @var bool
      */
-    private $griefing;
+    private $griefing = false;
+
+    public function __construct() {
+        $this->setCreationDate(new \DateTime());
+    }
 
     /**
      * @return string
@@ -77,6 +101,20 @@ class Overwatch {
      */
     public function setUserId($userId) {
         $this->userId = $userId;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreationDate() {
+        return $this->creationDate;
+    }
+
+    /**
+     * @param \DateTime $creationDate
+     */
+    public function setCreationDate($creationDate) {
+        $this->creationDate = $creationDate;
     }
 
     /**
@@ -147,5 +185,22 @@ class Overwatch {
      */
     public function setGriefing($griefing) {
         $this->griefing = $griefing;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMapPool() {
+        return $this->mapPool;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasValidMap() {
+        if (array_search($this->getMap(), $this->getMapPool()) !== false) {
+            return true;
+        }
+        return false;
     }
 }
