570ac37413f4193f933d68f36ea32c0e75513d39
Daniel Walldorf <d.walldorf@me.com>
2016-04-06 19:29:40 +0200

    removed unused doctrine bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,7 +12,6 @@ class AppKernel extends Kernel {
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
