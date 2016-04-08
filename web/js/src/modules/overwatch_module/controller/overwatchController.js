95e7501ba666063f058255039abb166a7502381f
Daniel Walldorf <d.walldorf@me.com>
2016-04-09 01:58:20 +0200

    reset overwatch form on submit

--- web/js/src/modules/overwatch_module/controller/overwatchController.js
+++ web/js/src/modules/overwatch_module/controller/overwatchController.js
@@ -28,6 +28,7 @@ angular.module('owTracker')
 
         $scope.submitOverwatch = function () {
             overwatchService.submitOverwatch($scope.newOverwatch).then(function (res) {
+                resetOverwatchForm();
                 $scope.overwatches.push(res.data);
             });
         };
