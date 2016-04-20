bda4f40cb819ff34cac3eee3e2ec204e6694f68e
Daniel Walldorf <d.walldorf@me.com>
2016-04-20 18:34:32 +0200

    js refactoring and cleanup

---
+++ web/js/src/modules/overwatch_module/controller/newOverwatchController.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('owTracker')
+    .controller('NewOverwatchController', ['$rootScope', '$scope', 'OverwatchService',
+        function ($rootScope, $scope, overwatchService) {
+
+            var newOverwatch = {
+                map: {id: 0},
+                overwatchDate: new Date(),
+                aimAssist: false,
+                visualAssist: false,
+                otherAssist: false,
+                griefing: false
+            };
+
+            function resetOverwatchForm() {
+                $scope.newOverwatch = angular.copy(newOverwatch);
+            }
+
+            $scope.submitOverwatch = function () {
+                overwatchService.submitOverwatch($scope.newOverwatch).then(function (res) {
+                    resetOverwatchForm();
+                    $scope.overwatches.push(res.data);
+                });
+            };
+
+            resetOverwatchForm();
+        }]
+    );
--- web/js/src/modules/overwatch_module/controller/overwatchController.js
+++ web/js/src/modules/overwatch_module/controller/overwatchController.js
@@ -4,52 +4,13 @@ angular.module('owTracker')
     .controller('OverwatchController', ['$rootScope', '$scope', 'OverwatchService',
         function ($rootScope, $scope, overwatchService) {
 
-            var newOverwatch = {
-                map: {id: 0},
-                overwatchDate: new Date(),
-                aimAssist: false,
-                visualAssist: false,
-                otherAssist: false,
-                griefing: false
-            };
-
-            function resetOverwatchForm() {
-                $scope.newOverwatch = angular.copy(newOverwatch);
-            }
-
             function init() {
-                resetOverwatchForm();
-
                 $scope.overwatches = [];
                 overwatchService.getOverwatchList().then(function (res) {
                     if (res.status == 200) {
                         $scope.overwatches = res.data;
                     }
                 });
-            }
-
-            $scope.submitOverwatch = function () {
-                overwatchService.submitOverwatch($scope.newOverwatch).then(function (res) {
-                    resetOverwatchForm();
-                    $scope.overwatches.push(res.data);
-                });
-            };
-
-            function getTodayDate() {
-                var today = new Date();
-                var day = today.getDate();
-                var month = today.getMonth() + 1; //January is 0!
-
-                var year = today.getFullYear();
-                if (day < 10) {
-                    day = '0' + day
-                }
-                if (month < 10) {
-                    month = '0' + month
-                }
-                today = month + '/' + day + '/' + year;
-
-                return today;
             }
 
             init();
