eb00efb59054c3344465f543ab50b0309e181ac7
Daniel Walldorf <d.walldorf@me.com>
2016-05-19 22:59:03 +0200

    showing errors during registration

--- web/app/user/register.component.ts
+++ web/app/user/register.component.ts
@@ -8,31 +8,52 @@ import {User} from "./model/user";
 @Component({
     templateUrl: 'app/user/views/register.html'
 })
-
 export class RegisterComponent {
+
+    private router: Router;
+    private userService: UserService;
 
     private user: User;
 
-    constructor(private userService: UserService, private router: Router) {
+    formErrors = [];
+
+    constructor(userService: UserService, router: Router) {
+        this.router = router;
+        this.userService = userService;
+
         this.user = new User();
+    }
+
+    ngOnInit() {
+        this.userService.getCurrentUser();
+        this.userService.userKnownEventEmitter.subscribe(() => this.goToLogin())
     }
 
     register() {
         this.userService.register(this.user).subscribe(
-            () => this.goToLogin(),
-            err => this.handleRegisterError(err)
+            () => this.handleRegisterSuccess(),
+            err => this.handleRegisterError(err.json())
         );
     }
 
-    handleRegisterError(err) {
-        console.error(err);
-
-        // TODO: implement
-        console.log('implement me!');
+    private goToLogin() {
+        this.router.navigate([ AppConfig.ROUTE_NAME_LOGIN ]);
     }
 
-    goToLogin() {
-        this.router.navigate([ AppConfig.ROUTE_NAME_LOGIN ])
+    private handleRegisterSuccess() {
+        this.formErrors = [];
+        this.goToLogin();
+    }
+
+    private handleRegisterError(err) {
+        this.formErrors = [];
+
+        if (err.hasOwnProperty('errors')) {
+            var errors = err.errors;
+            for (var key in errors) {
+                this.formErrors[ key ] = errors[ key ];
+            }
+        }
     }
 
 }
