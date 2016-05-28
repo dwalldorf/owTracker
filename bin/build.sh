6e8da57c48c376d3c5d8b1f1a778c9898021d6fc
Daniel Walldorf <d.walldorf@me.com>
2016-05-29 01:02:39 +0200

    updated build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -2,21 +2,8 @@
 
 cd /usr/share/nginx/owt
 
-echo
-echo "npm install..."
-echo
 npm install
-
-echo
-echo
-echo
-echo "composer install..."
-echo
 composer install
 
-echo
-echo
-echo
-echo "building frontend..."
-echo
 ./node_modules/ntypescript/bin/tsc --outDir web/app/dist
+./bin/console owt:createIndices
