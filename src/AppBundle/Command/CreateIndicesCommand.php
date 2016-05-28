9cfaf9005c544f8f2fc24dd2b6bb8fa1afcf2e18
Daniel Walldorf <d.walldorf@me.com>
2016-05-29 01:02:26 +0200

    command to create mongo indices

---
+++ src/AppBundle/Command/CreateIndicesCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateIndicesCommand extends BaseContainerAwareCommand {
+
+    protected function configure() {
+        $this->setName('owt:createIndices')
+            ->setHelp('Ensure mongo indices are created');
+    }
+
+    protected function executeCommand(InputInterface $input, OutputInterface $output) {
+        $dm = $this->container->get('doctrine.odm.mongodb.document_manager');
+        $dm->getSchemaManager()->ensureIndexes();
+
+        $this->output->writeln('[END] mongo indices created.');
+    }
+}
