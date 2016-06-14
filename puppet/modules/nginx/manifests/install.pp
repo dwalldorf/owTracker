ab9f81f2be329ae16fbd9219c13033e66f77559e
Daniel Walldorf <d.walldorf@me.com>
2016-06-14 19:31:48 +0200

    puppet - phpunit and upgrade to php5.6

--- puppet/modules/nginx/manifests/install.pp
+++ puppet/modules/nginx/manifests/install.pp
@@ -7,6 +7,6 @@ class nginx::install inherits nginx {
     }
     package{ 'apache2':
         ensure  => purged,
-        require => Package['php5', 'php5-cgi', 'php5-fpm', 'php5-intl'],
+        require => Package['php5.6-common', 'php5.6-cgi', 'php5.6-fpm', 'php5.6-intl'],
     }
 }
--- puppet/modules/php/manifests/install.pp
+++ puppet/modules/php/manifests/install.pp
@@ -1,28 +1,54 @@
 class php::install inherits php {
-    package { 'php5':
+    apt::ppa { 'ppa:ondrej/php': }
+
+    package { 'php5.6-common':
         ensure => latest,
     }
-    package { 'php5-cgi':
+    package{ 'php5.6-xml':
         ensure  => latest,
-        notify  => Service['php5-fpm'],
+        require => Package['php5.6-common'],
     }
-    package { 'php5-fpm':
-        ensure => latest,
-    }
-    package { 'php5-intl':
+    package{ 'php5.6-dom':
         ensure  => latest,
-        notify  => Service['php5-fpm'],
+        require => Package['php5.6-xml'],
+    }
+    package { 'php5.6-cgi':
+        ensure  => latest,
+        require => Package['php5.6-common'],
+        notify  => Service['php5.6-fpm'],
+    }
+    package { 'php5.6-fpm':
+        ensure  => latest,
+        require => Package['php5.6-common'],
+    }
+    package { 'php5.6-intl':
+        ensure  => latest,
+        require => Package['php5.6-common'],
+        notify  => Service['php5.6-fpm'],
     }
     package { 'memcached':
         ensure => latest,
     }
-    package { 'php5-memcache':
+    package { 'php-memcache':
         ensure  => latest,
-        require => Package['memcached'],
-        notify  => Service['php5-fpm'],
+        require => Package['memcached', 'php5.6-common'],
+        notify  => Service['php5.6-fpm'],
     }
-    package{ 'php-apc':
+    package{ 'php-xcache':
         ensure  => latest,
-        require => Package['php5'],
+        require => Package['php5.6-common'],
+        notify  => Service['php5.6-fpm'],
     }
+
+    exec { 'install_phpunit':
+        command => 'wget -q https://phar.phpunit.de/phpunit.phar && chmod +x phpunit.phar && mv phpunit.phar /usr/local/bin/phpunit',
+        require => Package['curl'],
+        user    => root,
+        creates => '/usr/local/bin/phpunit',
+        path    => ['/bin', '/usr/bin'],
+        cwd     => '/tmp',
+    }
+
+    Apt::Ppa['ppa:ondrej/php'] ->
+    Package['php5.6-common']
 }
