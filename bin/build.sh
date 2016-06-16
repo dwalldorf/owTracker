4ded645de1e47503ac535727acf728e95bb897ae
Daniel Walldorf <d.walldorf@me.com>
2016-06-16 20:04:32 +0200

    drilled out build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,18 +1,56 @@
 #!/bin/bash
 
-cd /usr/share/nginx/owt
+WORKING_DIR=/usr/share/nginx/owt
+ENVIRONMENT=dev
+API_PREFIX="/app_dev.php/api"
+
+cd ${WORKING_DIR}
+
+for i in "$@"
+do
+case $i in
+    -e=*|--env=*)
+    ENVIRONMENT="${i#*=}"
+    shift
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+echo "environment: $ENVIRONMENT"
+echo ""
+
+if [ ${ENVIRONMENT} == "prod" ]
+then
+    API_PREFIX="/api"
+
+    echo "checking if there are any debug statements.."
+    LDD_EXISTS=$(grep -nR " ldd(" ./app ./src)
+
+    if [[ ${LDD_EXISTS} != "" ]]
+    then
+        echo "[ERROR] found ldd:"
+        echo "$LDD_EXISTS"
+        exit 1
+    else
+        echo "ok"
+    fi
+fi
+
+echo "setting API_PREFIX to $API_PREFIX"
+sed -i "/static API_PREFIX = /c\    static API_PREFIX = '$API_PREFIX';" ./web/app/app.config.ts
 
 npm install
 composer install
 
 ./node_modules/ntypescript/bin/tsc --outDir web/app/dist
 
-rm -rf ./var/cache/*
-./bin/console cache:warmup --env=dev
-./bin/console cache:warmup --env=prod
+rm -rf ./var/cache/${ENVIRONMENT}
+./bin/console cache:warmup --env=${ENVIRONMENT}
 
-./bin/console assets:install --env=dev
-./bin/console assets:install --env=prod
+./bin/console assets:install --env=${ENVIRONMENT}
 
 ./bin/console owt:createIndices
 ./bin/console owt:memcache
