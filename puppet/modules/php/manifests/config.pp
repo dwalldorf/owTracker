ac2ecbc8ad84b3999d6e72f9588c3241133be37f
Daniel Walldorf <d.walldorf@me.com>
2016-06-28 23:11:18 +0200

    puppet php cli config

--- puppet/modules/php/manifests/config.pp
+++ puppet/modules/php/manifests/config.pp
@@ -30,4 +30,13 @@ class php::config inherits php {
         require => Package['php5.6-cgi'],
         notify  => Service['php5.6-fpm'],
     }
+    file { 'php5-cli_ini':
+        path    => '/etc/php/5.6/cli/php.ini',
+        source  => 'puppet:///modules/php/php-cli.ini',
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => 0644,
+        require => Package['php5.6-common'],
+    }
 }
