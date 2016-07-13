bfb42aa492847aafe306fdc364fb8ef12a2e82af
Daniel Walldorf <d.walldorf@me.com>
2016-07-14 00:08:16 +0200

    cronjob to read parsed demo info from queue

--- src/DemoBundle/Service/DemoInfoConsumer.php
+++ src/DemoBundle/Service/DemoInfoConsumer.php
@@ -3,6 +3,9 @@
 namespace DemoBundle\Service;
 
 use AppBundle\Service\BaseService;
+use AppBundle\Util\AppSerializer;
+use DemoBundle\Document\Demo;
+use DemoBundle\Form\DemoType;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 
@@ -17,11 +20,27 @@ class DemoInfoConsumer extends BaseService implements ConsumerInterface {
         $this->demoService = $this->getService(DemoService::ID);
     }
 
+    protected function createForm($type, $data = null, array $options = []) {
+        return $this->container->get('form.factory')->create($type, $data, $options);
+    }
+
     /**
      * @param AMQPMessage $msg
      * @return mixed
      */
     public function execute(AMQPMessage $msg) {
+        $demo = new Demo();
+        $demoArray = AppSerializer::getInstance()->toArray($msg->getBody());
+
+        /* @var $form \Symfony\Component\Form\Form */
+        $form = $this->container->get('form.factory')->create(DemoType::class, $demo);
+        $form->submit($demoArray);
+
+        if ($form->isValid()) {
+            $this->demoService->save($demo);
+            return true;
+        }
+
         return false;
     }
 }
